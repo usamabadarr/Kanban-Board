b317af599181b398e62f05e81b7727fea7a0ab84
usamabadarr <usamabadarr@gmail.com>
2024-12-03 18:29:28 -0500

    updated server.ts

--- server/src/server.ts
+++ server/src/server.ts
@@ -8,7 +8,7 @@ import { sequelize } from './models/index.js';
 dotenv.config();
 
 const app = express();
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 3000;
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
