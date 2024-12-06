45629ea225346dec21384bb00598c827c82e6729
Usama Badar <103148408+usamabadarr@users.noreply.github.com>
2024-12-05 20:15:02 -0500

    Update index.ts

--- server/src/models/index.ts
+++ server/src/models/index.ts
@@ -1,45 +1,25 @@
 import dotenv from 'dotenv';
+dotenv.config();
+
 import { Sequelize } from 'sequelize';
 import { UserFactory } from './user.js';
 import { TicketFactory } from './ticket.js';
 
-dotenv.config();
-
 const sequelize = process.env.DB_URL
-  ? new Sequelize(process.env.DB_URL, {
-      logging: console.log, 
+  ? new Sequelize(process.env.DB_URL)
+  : new Sequelize(process.env.DB_NAME || '', process.env.DB_USER || '', process.env.DB_PASSWORD, {
+      host: 'localhost',
+      // port: 5432, // Added port for deploying error
+      dialect: 'postgres',
       dialectOptions: {
         decimalNumbers: true,
       },
-    })
-  : new Sequelize(
-      process.env.DB_NAME || '', 
-      process.env.DB_USER || '', 
-      process.env.DB_PASSWORD || '',
-      {
-        host: '127.0.0.1', 
-        port: 5432, 
-        dialect: 'postgres',
-        logging: console.log, 
-        dialectOptions: {
-          decimalNumbers: true,
-        },
-      }
-    );
+    });
 
 const User = UserFactory(sequelize);
 const Ticket = TicketFactory(sequelize);
 
 User.hasMany(Ticket, { foreignKey: 'assignedUserId' });
-Ticket.belongsTo(User, { foreignKey: 'assignedUserId', as: 'assignedUser' });
-
-(async () => {
-  try {
-    await sequelize.authenticate();
-    console.log('Database connection established successfully.');
-  } catch (error) {
-    console.error('Unable to connect to the database:', error);
-  }
-})();
+Ticket.belongsTo(User, { foreignKey: 'assignedUserId', as: 'assignedUser'});
 
 export { sequelize, User, Ticket };
