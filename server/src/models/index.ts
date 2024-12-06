e74e03d2133f3c27abaf6d2b8c50966de3227abb
Usama Badar <103148408+usamabadarr@users.noreply.github.com>
2024-12-05 20:31:42 -0500

    Update index.ts

--- server/src/models/index.ts
+++ server/src/models/index.ts
@@ -6,20 +6,33 @@ import { UserFactory } from './user.js';
 import { TicketFactory } from './ticket.js';
 
 const sequelize = process.env.DB_URL
-  ? new Sequelize(process.env.DB_URL)
-  : new Sequelize(process.env.DB_NAME || '', process.env.DB_USER || '', process.env.DB_PASSWORD, {
-      host: 'localhost',
-      // port: 5432, // Added port for deploying error
+  ? new Sequelize(process.env.DB_URL, {
       dialect: 'postgres',
       dialectOptions: {
         decimalNumbers: true,
       },
-    });
+    })
+  : new Sequelize(
+      process.env.DB_NAME || '',
+      process.env.DB_USER || '',
+      process.env.DB_PASSWORD || '',
+      {
+        host: process.env.DB_HOST || 'localhost',
+        port: Number(process.env.DB_PORT) || 5432,
+        dialect: 'postgres',
+        dialectOptions: {
+          decimalNumbers: true,
+        },
+      }
+    );
 
+// Initialize models
 const User = UserFactory(sequelize);
 const Ticket = TicketFactory(sequelize);
 
+// Define relationships
 User.hasMany(Ticket, { foreignKey: 'assignedUserId' });
-Ticket.belongsTo(User, { foreignKey: 'assignedUserId', as: 'assignedUser'});
+Ticket.belongsTo(User, { foreignKey: 'assignedUserId', as: 'assignedUser' });
 
 export { sequelize, User, Ticket };
+
